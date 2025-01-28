1a5a187d471bd4bd39fbe96e4280e0b408c313cf
Juniard005 <jardi131gmail.com>
2025-01-28 17:48:42 +0700

    Alhamdullilah Akhinya Selesai

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
-use App\Http\Controllers\sendEmailController;
+use App\Models\santri;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
+use App\Http\Controllers\sendEmailController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/email/verify/{id}/{hash}', function (Request $request, Santri $santri) {
+    return $santri->setValidatedEmail($request);
+})->name('verification.verify');
