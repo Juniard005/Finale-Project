afe459572d38a9037fce784207324c8e662dfac7
Juniard005 <jardi131gmail.com>
2025-01-28 22:03:56 +0700

    Akhirnya Tugas Ku Selesai

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
 use App\Models\santri;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Request;
-use App\Http\Controllers\sendEmailController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/email/verify/{id}/{hash}', function (Request $request, Santri $santri) {
-    return $santri->setValidatedEmail($request);
-})->name('verification.verify');
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('/email/verify', function (Request $request, Santri $santri) {
+//         return $santri->setValidatedEmail($request);
+//     })->name('verification.verify');
+// });
+
