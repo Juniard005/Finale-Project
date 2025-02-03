e2f36c40ff5befde965ec1f53ae600e6e09cdd04
Juniard005 <jardi131gmail.com>
2025-02-03 22:14:24 +0700

    Nambah Fitur Hide Pada Upload Berkas

--- resources/views/filament/custom/upload-file-jadbel.blade.php
+++ resources/views/filament/custom/upload-file-jadbel.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div x-data="{ showUpload: false}">
     <x-filament::breadcrumbs :breadcrumbs="[
     '/admin/jad-bel' => 'Jadwal Belajar',
     '' => 'List',
@@ -9,7 +9,14 @@
             {{ $data }}
         </div>
     </div>
-    <div>
+
+    <button
+    @click="showUpload = !showUpload"
+    class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4">
+    Unggah Berkas
+    </button>
+
+    <div x-show="showUpload" class="mt-5">
         <form wire:submit="save" class="w-full max-w-sm flex mt-2">
             <div class="mb-4">
                 <label for="fileinput" class="block text-gray-700 text-sm font-bold-mb-2">
--- resources/views/filament/custom/upload-file.blade.php
+++ resources/views/filament/custom/upload-file.blade.php
@@ -1,15 +1,22 @@
-<div>
+<div x-data="{ showUpload: false }">
     <x-filament::breadcrumbs :breadcrumbs="[
     '/admin/santris' => 'Santri',
     '' => 'List',
-]" />
+    ]" />
     <div class="flex justify-between mt-1">
         <div class="font-bold text-3xl">Santri</div>
         <div>
             {{ $data }}
         </div>
     </div>
-    <div>
+
+    <button
+    @click="showUpload = !showUpload"
+    class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4">
+    Unggah Berkas
+    </button>
+
+    <div x-show="showUpload" class="mt-5">
         <form wire:submit="save" class="w-full max-w-sm flex mt-2">
             <div class="mb-4">
                 <label for="fileinput" class="block text-gray-700 text-sm font-bold-mb-2">
@@ -31,3 +38,5 @@
         </form>
     </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
